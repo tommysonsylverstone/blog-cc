29004cae8dc2790b0ae0fc2f31bda05c349cd440
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2022-11-01 17:42:21 +0100

    nevermind previous commit

---
+++ model/Model.class.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Model;
+
+use PDO, PDOException;
+
+abstract class Model
+{
+    private static $pdo;
+
+    private static function setBdd()
+    {
+        try {
+            self::$pdo = new PDO('mysql:host=localhost;dbname=partie_deux_blog;charset=utf8', 'root', '', array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+            ));
+        } catch (PDOException $e) {
+            //$e->getMessage();
+            die('Problème de connexion à la base de données.');
+        }
+    }
+    protected function getBdd()
+    {
+        if (self::$pdo === null) {
+            self::setBdd();
+        }
+        return self::$pdo;
+    }
+}
