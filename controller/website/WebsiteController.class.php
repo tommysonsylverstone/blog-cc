68016e53e20b47a3db46ce03c8bd330b11f66eec
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2022-11-01 19:44:43 +0100

    finished conditions and error messages on register form

--- controller/website/WebsiteController.class.php
+++ controller/website/WebsiteController.class.php
@@ -2,8 +2,17 @@
 
 namespace Controller\Website;
 
+use Model\Users;
+use Model\UsersManager;
+
 class WebsiteController
 {
+    private $usersManager;
+
+    public function __construct()
+    {
+        $this->usersManager = new UsersManager;
+    }
     public function home()
     {
         require('view/website/home.view.php');
@@ -11,6 +20,50 @@ class WebsiteController
 
     public function register()
     {
+        if (!empty($_POST)) {
+            foreach ($_POST as $key => $value) {
+                $_POST[$key] = htmlspecialchars(trim($value));
+            }
+            extract($_POST);
+            $errors = [];
+            if (!preg_match('/^\w{0,}$/', $username)) {
+                $errors[] = "Le pseudonyme ne peut que contenir que des lettres, chiffres et underscore ( _ ).";
+            }
+            if (strlen($username) > 30 || strlen($username) < 3) {
+                $errors[] = "Le pseudonyme doit faire entre 3 et 30 caractères.";
+            }
+            if (!preg_match('/^[a-zA-Z]{0,}$/', $firstname)) {
+                $errors[] = "Le prénom ne peut que contenir des lettres.";
+            }
+            if (strlen($firstname) > 30 || strlen($firstname) < 3) {
+                $errors[] = "Le prénom doit faire entre 3 et 30 caractères.";
+            }
+            if (!preg_match('/^[a-zA-Z]{0,}$/', $lastname)) {
+                $errors[] = "Le nom ne peut que contenir des lettres.";
+            }
+            if (strlen($lastname) > 30 || strlen($lastname) < 3) {
+                $errors[] = "Le nom doit faire entre 3 et 30 caractères.";
+            }
+            if ($password !== $confirmpwd) {
+                $errors[] = "Le mot de passe entré et le mot de passe confirmation ne correspondent pas.";
+            }
+            if (!preg_match('/^[\w@^#-]{0,}$/', $password)) {
+                $errors[] = "Le mot de passe ne peut que contenir que des lettres, chiffres et les caractères spéciaux suivants : @ ^ # - _.";
+            }
+            if (strlen($password) > 30 || strlen($password) < 8) {
+                $errors[] = "Le mot de passe doit faire entre 8 et 30 caractères.";
+            }
+
+            if (empty($errors)) {
+                var_dump("All clear");
+            }
+
+            var_dump($_POST, $errors);
+
+            die;
+            $user = new Users(null, $username, $firstname, $lastname, 'classic', $password);
+            $register = $this->usersManager->register($user);
+        }
         require('view/website/register.view.php');
     }
 
