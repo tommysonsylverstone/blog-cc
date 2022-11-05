77e96e6c95bcd424cd3908d540fb1f93058c8b20
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2022-11-05 17:27:35 +0100

    completed add function on PostsManager

--- model/PostsManager.class.php
+++ model/PostsManager.class.php
@@ -2,14 +2,32 @@
 
 namespace Model;
 
-class PostsManager extends Model {    
+class PostsManager extends Model
+{
     /**
      * add
      *
      * @param  Posts $post
      * @return void
      */
-    public function add(Posts $post) {
-        
+    public function add(Posts $post)
+    {
+        $id = $post->getId();
+        $title = $post->getTitle();
+        $content = $post->getContent();
+        $author = $post->getAuthor();
+        $publish_date = $post->getPublishDate();
+        $likes = $post->getLikes();
+
+        $sql = "INSERT INTO posts (post_id, title, content, author, publish_date, likes) VALUES(:post_id, :title, :content, :author, :publish_date, :likes)";
+        $req = Model::getBdd()->prepare($sql);
+        return $req->execute([
+            ":id" => $id,
+            ":title" => $title,
+            ":content" => $content,
+            ":author" => $author,
+            ":publish_date" => $publish_date,
+            ":likes" => $likes
+        ]);
     }
 }
