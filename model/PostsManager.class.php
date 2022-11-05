8556d29dd5eb4150acab2801d902a977cc605ca7
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2022-11-05 16:12:07 +0100

    created several views for articles

---
+++ model/PostsManager.class.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Model;
+
+class PostsManager extends Model {    
+    /**
+     * add
+     *
+     * @param  Posts $post
+     * @return void
+     */
+    public function add(Posts $post) {
+        
+    }
+}
--- view/admin/backoffice.view.php
+++ view/admin/backoffice.view.php
@@ -1,5 +1,8 @@
 <?php ob_start(); ?>
-
+    <ul>
+        <li><a href="?action=admin&p=list">Liste des articles</a></li>
+        <li><a href="?action=admin&p=add">Ajouter des articles</a></li>
+    </ul>
 <?php
 $content = ob_get_clean();
 $title = "Administration";
